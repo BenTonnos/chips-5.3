044e108884e6869dc1311856f7a561cf1054616f
Ben Tonnos <bentonnos1@berkeley.edu>
2026-07-18 00:16:04 +0000

    ratings feature

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,10 @@
 class Movie < ApplicationRecord
+
+  def self.all_ratings
+    return ['G','PG','PG-13','R']
+  end
+
+  def self.with_ratings(ratings_list)
+    return Movie.where(rating: ratings_list)
+  end
 end
